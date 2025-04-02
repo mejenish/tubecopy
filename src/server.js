d446fd3e78ee5da17afad9cc49968a9d886d6233
Jenish Bhandari <118617487+mejenish@users.noreply.github.com>
2025-04-02 21:22:25 +0545

    Environmental variable set

--- src/server.js
+++ src/server.js
@@ -1,5 +1,28 @@
-console.log("HELLO, World!");
-console.log("HELLO, World!");
-console.log("HELLO, World!");
-console.log("HELLO, World!");
-console.log("HELLO, World!");
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: ".env"
+})
+
+// const app = express();
+
+connectDB();
+
+// (async () => {
+//     try {
+//         const dbConnect = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//         app.on("error", (err) => {
+//             console.error("Error: ", err);
+//             throw err;
+//         })
+
+//         app.listen(process.env.PORT, () => {
+//             console.log(`App is listening on port ${process.env.PORT}`);
+//         })
+
+//     } catch (error) {
+//         console.error("Error: ", error)
+//         throw error;
+//     }
+// })
