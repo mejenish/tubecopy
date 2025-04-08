372debbb638570d5aee15c21c5c2acfceb9f8dd8
Jenish Bhandari <118617487+mejenish@users.noreply.github.com>
2025-04-08 20:56:37 +0545

    Schema Genereation

---
+++ src/models/user.models.js
@@ -0,0 +1,38 @@
+import mongoose, { Schema } from "mongoose";
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    avatar: {
+        type: String, //cloudinary url
+        required: true,
+    },
+    coverImage: {
+        type: String, //cloudinary url
+    },
+    watchHistory:[{
+        type: Schema.Types.ObjectId,
+        ref: "Video"
+    }]
+}, { timestamps: true });
+
+
+export const User = mongoose.model("User", userSchema);
