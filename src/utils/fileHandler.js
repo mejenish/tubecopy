c38216e61a3219d7b3ff0de81e5d735af366ddf6
Jenish Bhandari <118617487+mejenish@users.noreply.github.com>
2025-04-14 15:28:01 +0545

    Cloudinary Upload

--- src/utils/fileHandler.js
+++ src/utils/fileHandler.js
@@ -1,16 +1,24 @@
-import {v2 as cloudinary} from 'cloudinary';
+import { v2 as cloudinary } from 'cloudinary';
 import fs from "fs";
 
-    cloudinary.config({ 
-        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
-        api_key: process.env.CLOUDINARY_API_KEY, 
-        api_secret: process.env.CLOUDINARY_API_SECRET
-    });
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
-const cloudinary = async(localFilePath)=>{
+const uploadOnCloudinary = async (localFilePath) => {
     try {
-        if(!localFilePath) return null;
-        
+        if (!localFilePath) return null;
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        });
+        console.log("File is uploaded at Cloudinary", response.url);
+        return response;
     } catch (error) {
-        
-    }}
+        fs.unlinkSync(localFilePath); //remove the locally saved temp file as upload failed
+        return null;
+    }
+}
+
+export default uploadOnCloudinary;
