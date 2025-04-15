abc2bab5d86917bde814c109d63140e76f265c53
Jenish Bhandari <118617487+mejenish@users.noreply.github.com>
2025-04-15 21:05:17 +0545

    API Testing

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,9 @@
 import asyncHandler from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-    res.status(200).json({
-        message: "Success",
-    })
+    const { username, fullname, email, password } = req.body;
+    console.log("email: ", email + password);
 })
 
-export {registerUser};
+
+export { registerUser };
