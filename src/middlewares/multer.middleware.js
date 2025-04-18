033ec8ee67d29368c3639984d50517cb55fe18fe
Jenish Bhandari <118617487+mejenish@users.noreply.github.com>
2025-04-18 21:10:43 +0545

    Api Created

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,14 +1,16 @@
+import path from "path";
 import multer from "multer";
 
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname)
-    }
-  })
-  
-  export const upload = multer({ 
-    storage,
+  destination: function (req, file, cb) {
+    const tempPath = path.resolve("public", "temp");
+    cb(null, tempPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+export const upload = multer({ 
+  storage,
 })
