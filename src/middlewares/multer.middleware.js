6deb55936f39a644f6f2ea939a922b53e83ac437
Jenish Bhandari <118617487+mejenish@users.noreply.github.com>
2025-04-18 21:22:49 +0545

    multer fixed

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,16 +1,15 @@
-import path from "path";
 import multer from "multer";
 
+// Use an absolute path for the destination
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    const tempPath = path.resolve("public", "temp");
-    cb(null, tempPath);
+    cb(null, 'public/temp');  // Fixed to use an absolute path
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname);
   }
 });
 
-export const upload = multer({ 
+export const upload = multer({
   storage,
-})
+});
